a13c205aad6ea3cc98da8acacd45bf496930efe8
1Bc <mycodeisx@gmail.com>
2024-01-10 13:42:00 +0100

    implement add to cart functionality

--- src/main/java/com/example/shopwebapp/controller/ShopItemController.java
+++ src/main/java/com/example/shopwebapp/controller/ShopItemController.java
@@ -1,10 +1,14 @@
 package com.example.shopwebapp.controller;
 
 import com.example.shopwebapp.entity.Product;
+import com.example.shopwebapp.entity.ShoppingCart;
 import com.example.shopwebapp.service.ProductService;
+import com.example.shopwebapp.service.ShoppingCartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -14,9 +18,11 @@ import java.util.Locale;
 @Controller
 public class ShopItemController {
     private final ProductService productService;
+    private final ShoppingCartService shoppingCartService;
 
-    public ShopItemController(ProductService productService) {
+    public ShopItemController(ProductService productService, ShoppingCartService shoppingCartService) {
         this.productService = productService;
+        this.shoppingCartService = shoppingCartService;
     }
 
     @GetMapping("/shop/")
@@ -30,5 +36,19 @@ public class ShopItemController {
         model.addAttribute("productList", productList);
 
         return "shopItem/index";
+    }
+
+    @GetMapping("/shopItem/add")
+    public String add(Model model) {
+        model.addAttribute("shoppingCart", new ShoppingCart());
+        model.addAttribute("categoryOptions", productService.getCategoryOptions());
+        return "shopItem/add";
+    }
+
+    @PostMapping("/shopItem/add")
+    public String add(@ModelAttribute Product product) {
+        // TODO: replace with shoppingCartService:
+        shoppingCartService.addProduct(product);
+        return "redirect:/cart/";
     }
 }
